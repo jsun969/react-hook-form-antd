2a424829e328744aec5104e3bb307343b854be53
Justin Sun <i@jsun969.cn>
2023-04-08 23:11:54 +0800

    chore: set export files and update package config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm', 'iife'],
+  format: ['cjs', 'esm'],
   platform: 'browser',
-  legacyOutput: true,
   dts: true,
   splitting: true,
+  clean: true,
 });
