7b678a00cbc25813685a5267d7f1063bb6683429
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-10-27 07:37:31 +0200

    make use of rhf controller component

--- src/FormItem.tsx
+++ src/FormItem.tsx
@@ -1,7 +1,8 @@
 import { Form as AntdForm } from 'antd';
 import type React from 'react';
+import { Children, cloneElement, isValidElement } from 'react';
 import type { Control, FieldPath, FieldValues } from 'react-hook-form';
-import { useController } from 'react-hook-form';
+import { Controller, useController } from 'react-hook-form';
 
 type AntdFormItemProps = React.ComponentProps<typeof AntdForm.Item>;
 
@@ -9,7 +10,7 @@ export type FormItemProps<TFieldValues extends FieldValues = FieldValues> = {
   children: React.ReactNode;
   control: Control<TFieldValues>;
   name: FieldPath<TFieldValues>;
-} & Omit<AntdFormItemProps, 'name' | 'normalize' | 'rules' | 'validateStatus'>;
+} & Omit<AntdFormItemProps, 'name' | 'rules' | 'validateStatus'>;
 
 // TODO: Support `onBlur` `ref`
 // FIXME: `Touched` does not change in devtool
@@ -18,25 +19,34 @@ export const FormItem = <TFieldValues extends FieldValues = FieldValues>({
   control,
   name,
   help,
+  valuePropName,
   ...props
 }: FormItemProps<TFieldValues>) => {
-  const { field, fieldState } = useController({ name, control });
-
-  const handleNormalize: AntdFormItemProps['normalize'] = (value) => {
-    field.onChange(value);
-    return value;
-  };
+  const { fieldState } = useController({ name, control });
 
   return (
-    <AntdForm.Item
-      {...props}
+    <Controller
       name={name}
-      initialValue={field.value}
-      normalize={handleNormalize}
-      validateStatus={fieldState.invalid ? 'error' : undefined}
-      help={fieldState.error?.message ?? help}
-    >
-      {children}
-    </AntdForm.Item>
+      control={control}
+      render={({ field }) => (
+        <AntdForm.Item
+          {...props}
+          validateStatus={fieldState.invalid ? 'error' : undefined}
+          help={fieldState.error?.message ?? help}
+        >
+          {Children.map(
+            children,
+            (child) =>
+              isValidElement(child) &&
+              cloneElement(child, {
+                ...field,
+                ...(valuePropName === 'checked' && {
+                  checked: field.value,
+                }),
+              }),
+          )}
+        </AntdForm.Item>
+      )}
+    />
   );
 };
